8f319bbec8ea4993fca2d6421977837a4d5ae4e2
Steve Michelotti <steve.michelotti@gmail.com>
2013-09-16 15:42:14 -0500

    added Important Dates to case detail

--- AIS-CaseManagement/scripts/cases.js
+++ AIS-CaseManagement/scripts/cases.js
@@ -6,6 +6,7 @@
         var self = this;
         self.caseInfo = ko.observable();
         self.caseForms = ko.observableArray();
+        self.importantDates = ko.observableArray();
     };
     
     CasesViewModel = function() {
@@ -26,19 +27,21 @@
             self.selectedCase.caseInfo(item);
             app.application.navigate("#case-detail-view");
             app.api.getDocumentSummaryForCase(item.Id, function(data){
+                // Set Case Forms
                 var formsList = _.chain(data.d)
                     .filter(function(item) { return item.Status != "SerializedDocuments"; })
                     .map(function(item) { return item.ItemCount + " " + item.Status })
                     .value();
                 
                 self.selectedCase.caseForms(formsList);
-            });
             
-            //TODO: don't even need this call (remove it!)
-            // Just use OpeningDate from current item
-            // Then use momentjs to parse required dates to match other UI.
-            app.api.getCasesById(item.Id, function(data){
-               console.log("getcasesbyId", data); 
+                // Set Important Dates
+                var openingDt = moment(item.OpeningDate);
+                self.selectedCase.importantDates.push(openingDt.format("MM/DD/YYYY") + " - Case Opened");
+                var anniversaryDt = self.getAnniversaryDate(openingDt);
+                self.selectedCase.importantDates.push(anniversaryDt.format("MM/DD/YYYY") + " - Anuual Review");
+                var yearDiff = anniversaryDt.year() - openingDt.year();
+                self.selectedCase.importantDates.push(anniversaryDt.format("MM/DD/YYYY") + " - " + yearDiff + " year anniversary");
             });
         };
         
@@ -47,6 +50,15 @@
                 self.cases(data.d);
             });
         };
+        
+        // Private methods
+        self.getAnniversaryDate = function(opening){
+            var anniversaryThisYear = moment({ month: opening.month(), day: opening.date() });
+            if (moment().isAfter(anniversaryThisYear)){
+                anniversaryThisYear.add("years", 1);
+            }
+            return anniversaryThisYear;
+        }
         
         
         return {
